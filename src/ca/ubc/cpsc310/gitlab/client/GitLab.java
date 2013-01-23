4e9a2864dcbfb70a32e83833ecec4aa45e7ec2d7
Tom Jin <e2c8@ugrad.cs.ubc.ca>
2013-01-22 22:32:40 -0800

    GitLab.java of bonbon branch updated

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -49,70 +49,22 @@ public class GitLab implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
-	public void onModuleLoad() 
-	{
-		
-		service.getUsers(new AsyncCallback<List<IUser>>(){
+	public void onModuleLoad() {
+
+		service.getUsers(new AsyncCallback<List<IUser>>() {
 
 			@Override
 			public void onFailure(Throwable caught) {
-					Window.alert("Error occured " + caught.getClass() + " : " + caught.getMessage());
-				
+				Window.alert("Error occured " + caught.getClass() + " : "
+						+ caught.getMessage());
+
 			}
 
 			@Override
 			public void onSuccess(List<IUser> result) {
-			displayUsers(result);
-			}});
-	}
-	
-	public void uselessFunctions() {
-		// Let's loop a couple times
-		for (int i=0;i<19;i++) {
-			// Do nothing
-		}
-		
-	}
-	
-	/**
-	 * Used to display users 
-	 * @param users
-	 */
-	public void displayUsers(List<IUser> users)
-	{
-
-		RootPanel.get("root").add(flexTable);
-		
-		flexTable.setText(0,0, "Name");
-		
-		flexTable.setText(0,1,"Language");
-		flexTable.setText(0,2, "Shopping Cart Size");
-		flexTable.setText(0,3, "Wish List Size");
-		flexTable.setStyleName("centered-table", true);
-		
-		for(int i=0; i < users.size(); i++)
-		{
-			// Does something useless. Cuz GWT is slow enough already.
-			uselessFunctions();
-			
-			IUser user = users.get(i);
-			
-			flexTable.setText(i+1,0,user.getName());
-			if(user.getLanguage().trim().equals("EN"))
-			{
-				flexTable.setWidget(i+1, 1, new Image("uk.png"));
-			} else if (user.getLanguage().trim().equals("FR"))
-			{
-				flexTable.setWidget(i+1, 1, new Image("fr.png"));
-			} else
-			{
-				flexTable.setText(i+1,1,user.getLanguage());
+				Window.alert("Success occured");
 			}
-			
-			flexTable.setText(i+1,2,String.valueOf(user.getShoppingCart().size()));
-			
-			flexTable.setText(i+1,3,String.valueOf(user.getWishList().size()));
-		}
+		});
 	}
-	
+
 }
